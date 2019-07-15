0ee2b492bfe3335b4f1b5ab2b3378db147e179d2
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2019-07-15 14:55:12 +0200

    finished interface according to book

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,67 +1,3 @@
-// // NOTE: The contents of this file will only be executed if
-// // you uncomment its entry in "assets/js/app.js".
-
-// // To use Phoenix channels, the first step is to import Socket,
-// // and connect at the socket path in "lib/web/endpoint.ex".
-// //
-// // Pass the token on params as below. Or remove it
-// // from the params if you are not using authentication.
-// import {Socket} from "phoenix"
-
-// let socket = new Socket("/socket", {params: {token: window.userToken}})
-
-// // When you connect, you'll often need to authenticate the client.
-// // For example, imagine you have an authentication plug, `MyAuth`,
-// // which authenticates the session and assigns a `:current_user`.
-// // If the current user exists you can assign the user's token in
-// // the connection for use in the layout.
-// //
-// // In your "lib/web/router.ex":
-// //
-// //     pipeline :browser do
-// //       ...
-// //       plug MyAuth
-// //       plug :put_user_token
-// //     end
-// //
-// //     defp put_user_token(conn, _) do
-// //       if current_user = conn.assigns[:current_user] do
-// //         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-// //         assign(conn, :user_token, token)
-// //       else
-// //         conn
-// //       end
-// //     end
-// //
-// // Now you need to pass this token to JavaScript. You can do so
-// // inside a script tag in "lib/web/templates/layout/app.html.eex":
-// //
-// //     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-// //
-// // You will need to verify the user token in the "connect/3" function
-// // in "lib/web/channels/user_socket.ex":
-// //
-// //     def connect(%{"token" => token}, socket, _connect_info) do
-// //       # max_age: 1209600 is equivalent to two weeks in seconds
-// //       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
-// //         {:ok, user_id} ->
-// //           {:ok, assign(socket, :user, user_id)}
-// //         {:error, reason} ->
-// //           :error
-// //       end
-// //     end
-// //
-// // Finally, connect to the socket:
-// socket.connect()
-
-// // Now that you are connected, you can join channels with a topic:
-// let channel = socket.channel("topic:subtopic", {})
-// channel.join()
-//   .receive("ok", resp => { console.log("Joined successfully", resp) })
-//   .receive("error", resp => { console.log("Unable to join", resp) })
-
-// export default socket
-
 import {Socket} from "phoenix";
 
 const socket = new Socket("/socket", {});
@@ -86,21 +22,21 @@ function join(channel){
 
 join(game_channel);
 
-game_channel.on("said_hello", response => {
-    console.log("Returned Greeting:", response.message);
-})
+// game_channel.on("said_hello", response => {
+//     console.log("Returned Greeting:", response.message);
+// })
 
-function say_hello(channel, greeting){
-    channel.push("hello", {"message": greeting})
-        .receive("ok", response => {
-            console.log("Hello", response.message);
-        })
-        .receive("error", response => {
-            console.log("Unable to say hello to the channel.", response.message);
-        })
-}
+// function say_hello(channel, greeting){
+//     channel.push("hello", {"message": greeting})
+//         .receive("ok", response => {
+//             console.log("Hello", response.message);
+//         })
+//         .receive("error", response => {
+//             console.log("Unable to say hello to the channel.", response.message);
+//         })
+// }
 
-say_hello(game_channel, "World!");
+// say_hello(game_channel, "World!");
 
 // function leave(channel) {
 //     channel.leave()
@@ -113,5 +49,77 @@ say_hello(game_channel, "World!");
 // } 
 
 // leave(game_channel);
+
+function new_game(channel){
+    channel.push("new_game")
+        .receive("ok", response => {
+            console.log("New Game!", response);
+        })
+        .receive("error", response => {
+            console.log("Unable to start new game.", response);
+        })
+}
+
+new_game(game_channel)
+
+game_channel.on("player_added", response => {
+    console.log("Player Added", response)
+})
+
+function add_player(channel, player){
+    channel.push("add_player", player)
+        .receive("error", response => {
+            console.log("Unable to add new player: " + player, response);
+        })
+}
+
+add_player(game_channel, "diva")
+
+function position_island(channel, player, island, row, col){
+    var params = {"player": player, "island": island, "row": row, "col": col}
+    channel.push("position_island", params)
+        .receive("ok", response => {
+            console.log("Island positioned!", response);
+        })
+        .receive("error", response => {
+            console.log("Unable to position island.", response);
+        })
+}
+
+position_island(game_channel,   "player2",  "atoll",    1,  1)
+position_island(game_channel,   "player2",  "dot",      1,  5)
+position_island(game_channel,   "player2",  "l_shape",  1,  7)
+position_island(game_channel,   "player2",  "s_shape",  5,  1)
+position_island(game_channel,   "player2",  "square",   5,  5)
+position_island(game_channel,   "player1",  "dot",      1,  1)
+
+function set_islands(channel, player) {
+    channel.push("set_islands", player)
+       .receive("ok", response => {
+            console.log("Here is the board:");
+            console.dir(response.board);
+        })
+        .receive("error", response => {
+            console.log("Unable to set islands for: " + player, response)
+        })
+}
+
+game_channel.on("player_set_islands", response => {
+    console.log("Player Set Islands", response)
+})
+
+set_islands(game_channel, "player2")
+
+function guess_coordinate(channel, player, row, col) {
+    var params = {"player": player, "row": row, "col": col}
+    channel.push("guess_coordinate", params)
+        .receive("error", response => {
+            console.log("Unable to guess a coordinate: " + player, response)
+        })
+}
+
+game_channel.on("player_guessed_coordinate", response => {
+    console.log("Player Guessed Coordinate: ", response.result)
+})
 
 export default socket
